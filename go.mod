b3e0a798a83f73686c9e0c99582f16061e4c7ff4
Johnny Luo <johnnyluo1980@gmail.com>
2024-01-22 21:55:22 +1100

    add some keygen logic for ECDSA

--- go.mod
+++ go.mod
@@ -2,13 +2,15 @@ module github.com/johnnyluo/mobile-tss-lib
 
 go 1.21.1
 
-require github.com/bnb-chain/tss-lib/v2 v2.0.2
+require (
+	github.com/bnb-chain/tss-lib/v2 v2.0.2
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2
+)
 
 require (
 	github.com/agl/ed25519 v0.0.0-20200225211852-fd4d107ace12 // indirect
-	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
 	github.com/decred/dcrd/dcrec/edwards/v2 v2.0.3 // indirect
-	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
@@ -20,8 +22,6 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.16.0 // indirect
-	golang.org/x/mobile v0.0.0-20240112133503-c713f31d574b // indirect
-	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/tools v0.17.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
--- tss/common.go
+++ tss/common.go
@@ -1,14 +1,46 @@
 package tss
 
 import (
+	"crypto/elliptic"
+	"encoding/hex"
 	"errors"
 	"math"
+	"math/big"
+
+	tcrypto "github.com/bnb-chain/tss-lib/v2/crypto"
+	"github.com/btcsuite/btcd/btcec/v2"
 )
 
+// GetThreshold calculates the threshold value based on the input value.
+// It takes an integer value as input and returns the threshold value and an error.
+// If the input value is negative, it returns an error with the message "negative input".
 func GetThreshold(value int) (int, error) {
 	if value < 0 {
 		return 0, errors.New("negative input")
 	}
 	threshold := int(math.Ceil(float64(value)*2.0/3.0)) - 1
 	return threshold, nil
+}
+
+// IsOnCurve checks if the given point (x, y) lies on the elliptic curve.
+func IsOnCurve(x, y *big.Int) bool {
+	curve := btcec.S256()
+	return curve.IsOnCurve(x, y)
+}
+
+// GetHexEncodedECDSAPubKey returns the hexadecimal encoded string representation of an ECDSA public key.
+// It takes a pointer to an ECPoint as input and returns the encoded string and an error.
+// If the ECPoint is nil, it returns an empty string and an error indicating a nil ECPoint.
+// If the ECPoint is not on the curve, it returns an empty string and an error indicating an invalid ECPoint.
+func GetHexEncodedECDSAPubKey(pubKey *tcrypto.ECPoint) (string, error) {
+	if pubKey == nil {
+		return "", errors.New("nil ECPoint")
+	}
+
+	if !IsOnCurve(pubKey.X(), pubKey.Y()) {
+		return "", errors.New("invalid ECPoint")
+	}
+	ecdsaPubKey := pubKey.ToECDSAPubKey()
+	pubKeyBytes := elliptic.MarshalCompressed(ecdsaPubKey.Curve, ecdsaPubKey.X, ecdsaPubKey.Y)
+	return hex.EncodeToString(pubKeyBytes), nil
 }
