f75aebc305e0807d282e407431b11ebb468405d9
Johnny Luo <johnnyluo1980@gmail.com>
2024-01-22 22:25:40 +1100

    Add some EDDSA keygen logic

--- tss/local_state.go
+++ tss/local_state.go
@@ -1,11 +1,15 @@
 package tss
 
-import "github.com/bnb-chain/tss-lib/v2/ecdsa/keygen"
+import (
+	"github.com/bnb-chain/tss-lib/v2/ecdsa/keygen"
+	eddsaKeygen "github.com/bnb-chain/tss-lib/v2/eddsa/keygen"
+)
 
 // LocalState represent the information that will be saved locally
 type LocalState struct {
-	PubKey              string                    `json:"pub_key"`
-	LocalData           keygen.LocalPartySaveData `json:"local_data"`
-	KeygenCommitteeKeys []string                  `json:"keygen_committee_keys"`
-	LocalPartyKey       string                    `json:"local_party_key"`
+	PubKey              string                         `json:"pub_key"`
+	ECDSALocalData      keygen.LocalPartySaveData      `json:"ecdsa_local_data"`
+	EDDSALocalData      eddsaKeygen.LocalPartySaveData `json:"eddsa_local_data"`
+	KeygenCommitteeKeys []string                       `json:"keygen_committee_keys"`
+	LocalPartyKey       string                         `json:"local_party_key"`
 }
