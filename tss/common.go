34cd0f7f8d37d0c29f53c6891c2d56b880dfe95b
Johnny Luo <johnnyluo1980@gmail.com>
2024-01-23 15:58:38 +1100

    test

--- tss/common.go
+++ tss/common.go
@@ -8,7 +8,6 @@ import (
 	"math/big"
 
 	tcrypto "github.com/bnb-chain/tss-lib/v2/crypto"
-	"github.com/btcsuite/btcd/btcec/v2"
 )
 
 // GetThreshold calculates the threshold value based on the input value.
@@ -22,12 +21,6 @@ func GetThreshold(value int) (int, error) {
 	return threshold, nil
 }
 
-// IsOnCurve checks if the given point (x, y) lies on the elliptic curve.
-func IsOnCurve(x, y *big.Int) bool {
-	curve := btcec.S256()
-	return curve.IsOnCurve(x, y)
-}
-
 // GetHexEncodedPubKey returns the hexadecimal encoded string representation of an ECDSA/EDDSA public key.
 // It takes a pointer to an ECPoint as input and returns the encoded string and an error.
 // If the ECPoint is nil, it returns an empty string and an error indicating a nil ECPoint.
@@ -37,13 +30,16 @@ func GetHexEncodedPubKey(pubKey *tcrypto.ECPoint) (string, error) {
 		return "", errors.New("nil ECPoint")
 	}
 
-	if !IsOnCurve(pubKey.X(), pubKey.Y()) {
+	if !pubKey.IsOnCurve() {
 		return "", errors.New("invalid ECPoint")
 	}
 	ecdsaPubKey := pubKey.ToECDSAPubKey()
 	pubKeyBytes := elliptic.MarshalCompressed(ecdsaPubKey.Curve, ecdsaPubKey.X, ecdsaPubKey.Y)
 	return hex.EncodeToString(pubKeyBytes), nil
 }
+
+// Contains checks if a given string slice contains a specific search term.
+// It iterates through the slice and returns true if the search term is found, false otherwise.
 func Contains(s []string, searchterm string) bool {
 	for _, item := range s {
 		if item == searchterm {
@@ -53,6 +49,10 @@ func Contains(s []string, searchterm string) bool {
 	return false
 }
 
+// HashToInt converts a byte slice hash to a big.Int value using the provided elliptic curve.
+// If the length of the hash is greater than the orderBytes of the curve, it truncates the hash.
+// It then performs a right shift on the resulting big.Int value to ensure it fits within the orderBits of the curve.
+// The converted big.Int value is returned.
 func HashToInt(hash []byte, c elliptic.Curve) *big.Int {
 	orderBits := c.Params().N.BitLen()
 	orderBytes := (orderBits + 7) / 8
