ccfcf143a9a53a93f61ef008008c7e04b67fccbc
Johnny Luo <johnnyluo1980@gmail.com>
2024-02-01 21:55:25 +1100

    some changes to the interface

--- tss/request_response.go
+++ tss/request_response.go
@@ -1,18 +1,30 @@
 package tss
 
+import "strings"
+
 type KeygenRequest struct {
 	LocalPartyID string
-	AllParties   []string
+	AllParties   string
 }
+
+func (r KeygenRequest) GetAllParties() []string {
+	return strings.Split(r.AllParties, ",")
+}
+
 type KeygenResponse struct {
 	PubKey string `json:"pub_key"`
 }
 type KeysignRequest struct {
-	PubKey               string   `json:"pub_key"`
-	MessageToSign        string   `json:"message_to_sign"` // base64 encoded message that need to be signed
-	KeysignCommitteeKeys []string `json:"keysign_committee_keys"`
-	LocalPartyKey        string   `json:"local_party_key"`
+	PubKey               string `json:"pub_key"`
+	MessageToSign        string `json:"message_to_sign"` // base64 encoded message that need to be signed
+	KeysignCommitteeKeys string `json:"keysign_committee_keys"`
+	LocalPartyKey        string `json:"local_party_key"`
 }
+
+func (r KeysignRequest) GetKeysignCommitteeKeys() []string {
+	return strings.Split(r.KeysignCommitteeKeys, ",")
+}
+
 type Signature struct {
 	Msg        string `json:"msg"`
 	R          string `json:"r"`
